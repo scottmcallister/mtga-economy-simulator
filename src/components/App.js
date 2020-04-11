1dc0193c07e5c46a032d638624f2210a270bb0a8
Scott McAllister <smcallister@cvent.com>
2020-04-11 14:36:04 -0300

    update grid and add buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled'
+import {
+  initializeRareCollection,
+  countRares
+} from '../utils/simulation';
 
 const Grid = styled.div({
-    display: 'flex',
-    flexWrap: 'wrap',
-    alignItems: 'center'
-  })
+  display: 'flex',
+  flexWrap: 'wrap',
+  alignItems: 'center'
+});
 const Card = styled.div({
-  flexBasis: '250px',
+  flexBasis: '300px',
   flexGrow: 1,
   margin: '10px 10px',
   display: 'flex',
@@ -18,13 +22,43 @@ const Card = styled.div({
   borderRadius: '10px',
   color: '#FEFEFC',
   backgroundColor: '#4D4F4E'
-})
+});
+const MenuItem = styled.div({
+  flexBasis: '300px',
+  flexGrow: 1,
+  margin: '10px 10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-evenly',
+  flexDirection: 'column',
+  padding: '20px',
+  height: '100px',
+  borderRadius: '10px',
+  color: '#FEFEFC',
+  flexShrink: 0,
+  backgroundColor: '#324666'
+});
+const SubmitButton = styled.button({
+  backgroundColor: 'darkorange',
+  color: "#FEFEFC",
+  borderRadius: '5px',
+  padding: '8px',
+  border: 'none'
+});
+const ResetButton = styled.button({
+  backgroundColor: '#6493de',
+  color: "#FEFEFC",
+  borderRadius: '5px',
+  padding: '8px',
+  border: 'none'
+});
 
 function App() {
   const [gold, setGold] = useState(100000);
   const [gems, setGems] = useState(0);
   const [packs, setPacks] = useState(0);
   const [wildcards, setWildcards] = useState(0);
+  const [rares, setRares] = useState(initializeRareCollection());
   const [wcCounter, setWcCounter] = useState(0);
   const [winRate, setWinrate] = useState(0.5);
 
@@ -45,13 +79,35 @@ function App() {
           <div>{gems}</div>
         </Card>
         <Card>
+          <div>Wildcards</div>
+          <div>{wildcards}</div>
+        </Card>
+        <Card>
+          <div>Wildcard Counter</div>
+          <div>{wcCounter}/6</div>
+        </Card>
+        <Card>
           <div>Packs</div>
           <div>{packs}</div>
         </Card>
         <Card>
-          <div>Wildcards</div>
-          <div>{wildcards}</div>
+          <div>Rares</div>
+          <div>{countRares(rares)}</div>
         </Card>
+      </Grid>
+      <Grid>
+        <MenuItem>
+          <SubmitButton>Buy Pack</SubmitButton>
+        </MenuItem>
+        <MenuItem>
+          <SubmitButton>Open Pack</SubmitButton>
+        </MenuItem>
+        <MenuItem>
+          <SubmitButton>Draft</SubmitButton>
+        </MenuItem>
+        <MenuItem>
+          <ResetButton>Reset</ResetButton>
+        </MenuItem>
       </Grid>
     </Wrapper>
   );
